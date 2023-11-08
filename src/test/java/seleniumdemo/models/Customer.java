a384ad5c6e9ebae23bc2908dce1af987d0a0b7aa
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-08 22:30:07 +0100

    Creating Customer class

---
+++ src/test/java/seleniumdemo/models/Customer.java
@@ -0,0 +1,96 @@
+package seleniumdemo.models;
+
+public class Customer {
+
+    private String firstName = "Lukasz";
+    private String lastName = "Test";
+    private String companyName = "LEM";
+    private String country = "Poland";
+    private String street = "Drogowa";
+    private String flatNumber = "218";
+    private String zipCode = "44-111";
+    private String city = "Testowe";
+    private String phone = "123456789";
+    private String email = "testowyemail@gmail.com";
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(String flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
