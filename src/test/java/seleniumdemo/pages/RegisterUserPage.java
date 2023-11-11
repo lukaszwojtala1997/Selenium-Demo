946e99e57c00a935afed7c23db7131fc2cdd5ba3
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-11 22:36:29 +0100

    Creating RegisterUserPage

---
+++ src/test/java/seleniumdemo/pages/RegisterUserPage.java
@@ -0,0 +1,25 @@
+package seleniumdemo.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import seleniumdemo.utils.SeleniumHelper;
+
+public class RegisterUserPage {
+
+    @FindBy(linkText = "Dashboard")
+    private WebElement dashBoardLink;
+
+    WebDriver driver;
+
+    public RegisterUserPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public String getDashBoardLink() {
+        SeleniumHelper.waitForElementToBeVisible(driver, dashBoardLink);
+        return dashBoardLink.getText();
+    }
+}
