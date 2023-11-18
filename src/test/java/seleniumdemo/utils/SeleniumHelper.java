1fa9fc0377d800c7ae2b298039058dde9ddc83d9
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-18 13:27:54 +0100

    Adding method to take screenshot

--- src/test/java/seleniumdemo/utils/SeleniumHelper.java
+++ src/test/java/seleniumdemo/utils/SeleniumHelper.java
@@ -1,11 +1,14 @@
 package seleniumdemo.utils;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.MediaEntityModelProvider;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class SeleniumHelper {
@@ -19,9 +22,19 @@ public class SeleniumHelper {
         WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
         webDriverWait.until(ExpectedConditions.visibilityOf(locator));
     }
-    public static void presentOfElementLocation(WebDriver webDriver, By locator){
-        WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
-        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(locator));
+
+    public static MediaEntityModelProvider getScreenshot(WebDriver driver) throws IOException {
+        String path = takeScreenshot(driver);
+        return MediaEntityBuilder.createScreenCaptureFromPath(path).build();
+    }
+
+    private static String takeScreenshot(WebDriver driver) throws IOException {
+        int randomNumber = (int) (Math.random()*1000);
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        String path = "src/test/resources/screenshots/screenshot" + randomNumber + ".png";
+        FileUtils.copyFile(file, new File(path));
+        return path;
     }
 
 }
